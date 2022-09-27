76ad4fcf875ada41718738dc62603a91422084b5
Shammi141 <jannatulshammi181@gmail.com>
2022-09-28 02:04:14 +0600

    stroed data in localstorage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -12,19 +13,57 @@ const Shop = () => {
     const [cart, setCart]= useState([]);
 
     useEffect(() =>{
+        // console.log('products loaded before fetch')
         fetch('products.json')
         .then(res => res.json())
         // .then(data => console.log(data))
-        .then(data => setProducts(data))
+        .then(data => {
+            setProducts(data);
+            // console.log('products loaded');
+        })
 
     }, []);
 
+
+//localStorage theke load kore kisu thakle show korbe nathakle empty dekhabe
+    useEffect(() => {
+        //  console.log('localstorages first line', products);
+        const  storedCart = getStoredCart();
+        // console.log(storedCart);
+        const savedCart = [];
+        for(const id in storedCart){
+            // console.log(id);
+            const addedProduct = products.find(product => product.id === id);       //returns first element
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                // console.log(addedProduct);
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+        // console.log('local storage finished')
+    }, [products]);     //jehetu running hoy asynchoronous vabe so ja age load hote pare hobe. [products] deyar cz = [] empty pele ekbar e load korbe porer products gula r pabe na..bt we want products er value change hole call korte. called dependency
+
+
 //for adding clicked item in cart
-    const handleAddToCart = (product) => {
-        console.log(product);       //checkeh that items info is getting or not
+    const handleAddToCart = (selectedProduct) => {
+        //console.log(selectedProduct);       //checkeh that items info is getting or not
         // cart.push(product);    evabe add kokhonoi korbo na 
-        const newCart = [...cart, product];
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+        //newCart = [...cart, selectedProduct];     //ager cart e jodi kisu thakle tahole clicked product ta tar sathe add kore dibe order summary te
         setCart(newCart);
+        addToDb(selectedProduct.id)     //to storing data in localStorage from fakeDb.js
     }
     return (
         <div className='shop-container'>
